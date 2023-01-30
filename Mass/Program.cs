447dfbb6c3108fd67fc4a8f3b21aebed7c576dee
Artur <fox9910@gmail.com>
2023-01-30 21:39:25 +0500

    2 пункт задачи

--- Mass/Program.cs
+++ Mass/Program.cs
@@ -8,3 +8,13 @@ Console.WriteLine("Создание одномерного массива. ");
 Console.Write("Введите количество строк массива:  ");
 int rowcount = Convert.ToInt32(Console.ReadLine());
 string [] array = new string [rowcount]; //1 создание размера
+
+fillArray(array);
+
+void fillArray(string[] array)         // 2 заполнение массива с клавиатуры
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine(); 
+    }
+}
